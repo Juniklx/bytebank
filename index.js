dbc4c830dc6b5e7f77f86dd3d8bf926da5998902
Juniklx <marcelostj2014@gmail.com>
2026-08-04 15:27:17 -0300

    Adicionando arquivo inicial

---
+++ index.js
@@ -0,0 +1,11 @@
+const cliente1Nome = "Marcelo";
+const cliente1CPF = "11122233309";
+const cliente1Agencia = "1001";
+const cliente1Saldo = 0;
+
+const cliente2Nome = "Diana";
+const cliente2CPF = "22233344408";
+const cliente2Agencia = "1001";
+const cliente2Saldo = 0;
+
+console.log(cliente1Nome);
